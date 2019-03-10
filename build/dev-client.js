137080e8f6fa87aeb56c5571e310b4304328c5e9
Zafar Saleem <zafarsaleem3@gmail.com>
2019-03-10 10:07:00 -0400

    Update dev-client.js

--- build/dev-client.js
+++ build/dev-client.js
@@ -1,8 +1,8 @@
 const hotClient = require('webpack-hot-middleware/client');
 
 hotClient.subscribe((event) => {
-    if (event.action === 'reload') {
-        window.location.reload();
-    }
+  if (event.action === 'reload') {
+    window.location.reload();
+  }
 });
 
